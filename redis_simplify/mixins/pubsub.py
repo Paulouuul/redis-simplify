0ff7293d6fbe1ffe2868c634f1d69fa620c20536
Paulouuul <tebetpaulo91@yahoo.com>
2026-06-26 18:57:38 -0300

    refactor(PubSubMixin): removed runing flag and add stop_event variable

--- redis_simplify/mixins/pubsub.py
+++ redis_simplify/mixins/pubsub.py
@@ -1,5 +1,6 @@
 import logging
 import threading
+import json
 from typing import Callable
 
 from redis_simplify.mixins.decorator_metrics import recorded
@@ -27,7 +28,6 @@ class PubSubMixin:
     
     def publish_json(self, channel: str, data: dict) -> int:
         """Publica JSON em canal"""
-        import json
         return self.publish(channel, json.dumps(data))
     
     def subscribe(self, channel: str, callback: Callable, pattern: bool = False):
@@ -43,13 +43,13 @@ class PubSubMixin:
         else:
             pubsub.subscribe(channel)
         
-        # Flag para controlar o loop
-        running = True
+        # Variável para controlar o loop
+        stop_event = threading.Event()
         
         def listener():
             try:
                 for message in pubsub.listen():
-                    if not running:
+                    if stop_event.is_set():
                         break
                     if message['type'] in ('message', 'pmessage'):
                         data = message.get('data')
@@ -69,25 +69,30 @@ class PubSubMixin:
         
         # Armazena para fechar depois
         self._pubsubs.append(pubsub)
-        self._pubsub_threads.append((thread, pubsub, lambda: running))
+        self._pubsub_threads.append((thread, pubsub, stop_event))
         
         logger.info(f"Subscribed to {channel}")
         return pubsub
     
     def close_pubsubs(self):
         """Fecha todas as subscriptions ativas"""
-        for pubsub in self._pubsubs:
-            try:
-                pubsub.close()
-            except:
-                pass
-        self._pubsubs.clear()
+        for thread, pubsub, stop_event in self._pubsub_threads:
+            stop_event.set()  # Sinaliza para parar
         
         # Aguarda threads terminarem
-        for thread, pubsub, _ in self._pubsub_threads:
+        for thread, pubsub, stop_event in self._pubsub_threads:
             try:
                 if thread.is_alive():
                     thread.join(timeout=0.5)
             except:
                 pass
+        
+        # Fecha pubsubs
+        for pubsub in self._pubsubs:
+            try:
+                pubsub.close()
+            except:
+                pass
+        
+        self._pubsubs.clear()
         self._pubsub_threads.clear()
